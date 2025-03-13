7ac3ed85cb4927a170eae236708dce7f45fe0fee
Aleksandar Bokun <aleksandar1987b@gmail.com>
2025-03-13 23:01:00 +0100

    Function that returns a computers random value for rock, paper, scissors

---
+++ rps.js
@@ -0,0 +1,15 @@
+
+
+
+function getComputerChoice(num) {
+    let num = Math.floor(Math.random() * 3);
+
+    if (num === 0) {
+        console.log('rock');
+    } else if (num === 1) {
+        console.log('paper');
+    } else {
+        console.log('scissors');
+    }
+}
+getComputerChoice(num);
